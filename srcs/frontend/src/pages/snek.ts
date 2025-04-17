117d40ae6d027b3df8339e066cf57b9fb38fb54d
Julius De Baaij <jde-baai@student.codam.nl>
2025-04-17 17:27:43 +0200

    setup

--- srcs/frontend/src/pages/snek.ts
+++ srcs/frontend/src/pages/snek.ts
@@ -4,13 +4,17 @@ export function setupSnek() {
 	const root = document.getElementById('app');
 	if (root) {
 		root.innerHTML = "";
-		root.insertAdjacentHTML("beforeend", `
+		root.insertAdjacentHTML("beforeend", /*html*/ `
 			<link rel="stylesheet" href="src/styles/userMain.css">
-			<div class="overlay"></div>
-					<dropdown-menu></dropdown-menu>
+			<dropdown-menu></dropdown-menu>
 			
 			<div class="middle">
+			<div class="loader">
+				<progress value="0" max"100"></progress>
 			</div>
+			<canvas tabindex="0" id="canvas" width="1920" height"1080"></canvas>
+			</div>
+			<div>
 			`)
 	}
 	else {
