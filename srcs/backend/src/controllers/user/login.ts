563286043a5fc19edc3dd265682f9b0d32016373
Julius De Baaij <jde-baai@student.codam.nl>
2025-05-05 15:12:48 +0200

    added alias to session

--- srcs/backend/src/controllers/user/login.ts
+++ srcs/backend/src/controllers/user/login.ts
@@ -51,6 +51,7 @@ export const loginUser = async (request: FastifyRequest, reply: FastifyReply) =>
 		const updatedUser = await db.select().from(usersTable).where(eq(usersTable.username, username));
 		const pubUser = toPublicUser(updatedUser[0]);
 		request.session.set('uuid', updatedUser[0].uuid);
+		request.session.set('alias', updatedUser[0].alias);
 		return reply.code(200).send(pubUser);
 	} catch (error) {
 		request.log.error('loginUser failed:', error);
