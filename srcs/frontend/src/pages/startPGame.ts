0357cfd5ae74c6523a1e539ff310e6bf2f6e3fe6
Acco Bartelds <abarteld@f1r3s20.codam.nl>
2025-06-02 14:59:43 +0200

    alarm

--- srcs/frontend/src/pages/startPGame.ts
+++ srcs/frontend/src/pages/startPGame.ts
@@ -14,15 +14,15 @@ export function setupStartGame() {
 		<div class="overlay"></div>
 		<dropdown-menu></dropdown-menu>
 		
-		<div class="middle">
+		<div class="middle" id="middle">
 			<div class="container">
 				<h1 class="header" data-i18n="Game_Header"></h1>
 					
 				<div class="buttons">
-					<button class="btn" data-i18n="btn_Tournament"></button>
+					<button class="btn" id ="btn_1v1" data-i18n="btn_1v1"></button>
 				</div>
 				<div class="buttons">
-					<button class="btn" id ="btn_1v1" data-i18n="btn_1v1"></button>
+					<button class="btn" data-i18n="btn_Tournament"></button>
 				</div>
 			</div>
 		</div>
@@ -34,23 +34,21 @@ export function setupStartGame() {
 		setupNavigation();
 
 // Add event listener to launch Pong game
-const soloBtn = document.getElementById("btn_1v1");
-soloBtn?.addEventListener("click", () => {
-console.log("clicked SOLO");
-root.innerHTML = `
-<canvas id="renderCanvas" style="width: 100vw; height: 100vh; display: block;"></canvas>
-`;
+const Btn1v1 = document.getElementById("btn_1v1");
+Btn1v1?.addEventListener("click", () => {
+const middle = document.getElementById("middle");
+if (middle)
+	middle.innerHTML = `<canvas id="renderCanvas" style="position:relative; width: 100vw; top:140px; left:220px; height: 100vh; display: block;"></canvas>`;
 
 const canvas = document.getElementById("renderCanvas") as HTMLCanvasElement;
 if (canvas) {
 // const options:scene = []
 // const game = new Pong(canvas, options);
 const game = new Pong(canvas);
-game.run(); // You can also pass options if needed
+game.run();
 }
 });
 	}
 }
 
-// Needs A Bunch Of Modification
 
