a9d053b1f76bc091a9660330fbe17c569ffcee06
Julius De Baaij <jde-baai@student.codam.nl>
2025-06-06 18:21:32 +0200

    scrollable css with fonts, buttons

--- srcs/frontend/src/pages/matchMaking.ts
+++ srcs/frontend/src/pages/matchMaking.ts
@@ -13,13 +13,34 @@ export function setupMatchMaking() {
 		<link rel="stylesheet" href="src/styles/contentPages.css"> <!-- Link to the CSS file -->
 			<dropdown-menu></dropdown-menu>
 			<div class="middle">
-			<button class="game-btn-full" id="SnekHistory">
-				<span data-i18n="SwitchGame"></span> <img src="src/Pictures/game-snek.png">
-			</button>
-				<p> hello </p>
+				<h2 class="h2"> This is content that can be put outside the content area </h2>
+				<button class="btnFullWidth secondary">
+					<span>Full width secondary button</span>
+				</button>
+				<button class="btnFullWidth secondary">
+					<span>Full width secondary button</span>
+				</button>
 				<div class="contentArea">
-					<p> inside the content Area </p>
+					<h1 class="h1">This is the H1 font</h1>
+					<h2 class="h2">This is the H2 font</h2>
+					<button class="cbtn">
+						<span>default button</span> 
+					</button>
+					<h3 class="h3">This is the H3 font</h3>
+					<p class="p1">p1 inside the content Area</p>
+					<button class="cbtn secondary">
+						<span>the default button --- grows with size</span> 
+					</button>
+					<p class="p2">p2 inside the content Area</p>
+					<button class="btnFullWidth">
+						<span>always has max width</span> 
+					</button>
+					<p class="p3">p3 inside the content Area</p>
 				</div>
+				<p class="p1">p1 below the content Area</p>
+				<button class="btnFullWidth">
+					<span>Full width primary button</span>
+				</button>
 			</div>
 		</div>
 		`);
