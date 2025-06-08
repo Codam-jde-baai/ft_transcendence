baaf2e7f32b824cb766fbcf0038503570389372d
Julius <julius@debaaij.com>
2025-06-08 14:50:03 +0200

    game selection menu updated

--- srcs/frontend/src/pages/startPGame.ts
+++ srcs/frontend/src/pages/startPGame.ts
@@ -8,22 +8,29 @@ export function setupStartGame() {
 	if (root) {
 		root.innerHTML = "";
 		root.insertAdjacentHTML("beforeend", /*html*/`
-		<link rel="stylesheet" href="src/styles/startGame.css"> <!-- Link to the CSS file -->
+		<link rel="stylesheet" href="src/styles/contentPages.css"> 
 		<div class="overlay"></div>
 		<dropdown-menu></dropdown-menu>
-		
 		<div class="middle">
-			<div class="container">
-				<h1 class="header" data-i18n="Game_Header"></h1>
-					
+			<div class="flex flex-row space-x-6">
+				<img src="src/Pictures/game-pong.png" style="width: 100px; height: 100px;">
+				<img src="src/Pictures/game-snek.png" style="width: 100px; height: 100px;">
+			</div>
+			<label class="toggleSwitch">
+				<input type="checkbox">
+				<span class="toggle-option" data-i18n="btn_PlayPong"></span>
+				<span class="toggle-option" data-i18n="btn_PlaySnek"></span>
+			</label>
+			<div class="contentArea">
+				<h2 class="h2" data-i18n="Game_Header"></h2>
 				<div class="buttons">
-					<button class="btn" data-i18n="btn_Friend"></button>
+					<button class="btn" data-i18n="QuickPlay"></button>
 				</div>
 				<div class="buttons">
-					<button class="btn" data-i18n="btn_Match"></button>
+					<button class="btn" data-i18n="Tournament"></button>
 				</div>
 				<div class="buttons">
-					<button class="btn" data-i18n="btn_Solo"></button>
+					<button class="btn" data-i18n="MatchMaking"></button>
 				</div>
 			</div>
 		</div>
